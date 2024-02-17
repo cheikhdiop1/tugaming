58a1af7398f7b82aa9169c29d4fe2e6e9e40fc5f
cheikhdiop1 <cheikh_diop1@outlook.com>
2024-02-16 23:40:43 -0500

    Up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import GamePage from './components/GamePage';
+// Import other necessary components
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/game/:id" component={GamePage} />
+          {/* You can add more routes here as needed */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
